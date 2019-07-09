39116581d7b1e0e8d60067d13a74cdd799ccce7c
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2019-07-09 06:42:39 +0000

    上長ユーザー追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190709014408) do
+ActiveRecord::Schema.define(version: 20190709063632) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -33,11 +33,12 @@ ActiveRecord::Schema.define(version: 20190709014408) do
     t.boolean "admin", default: false
     t.string "department"
     t.datetime "basic_time", default: "2019-02-19 22:30:00"
-    t.datetime "work_start_time", default: "2019-02-19 23:00:00"
+    t.datetime "work_time", default: "2019-02-19 23:00:00"
     t.string "remember_digest"
     t.datetime "work_end_time", default: "2019-02-20 08:00:00"
     t.string "employee_number"
     t.string "uid"
+    t.boolean "superior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
