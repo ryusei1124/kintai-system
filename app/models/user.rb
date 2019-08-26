2cb638cdad4e4ffc5c385153f566de54d2e8683b
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2019-08-26 12:36:22 +0000

    d

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,4 +41,21 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  # #csvインポート
+  # def self.import(file)
+  #   CSV.foreach(file.path, headers: true) do |row|
+  #     # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
+  #     user = find_by(id: row["id"]) || new
+  #     # CSVからデータを取得し、設定する
+  #     user.attributes = row.to_hash.slice(*updatable_attributes)
+  #     # 保存する
+  #     user.save
+  #   end
+  # end
+  
+  # # 更新を許可するカラムを定義
+  # def self.updatable_attributes
+  #   ["name", "email", "department", "employee_number", "uid", "basic_time", "work_time", "work_end_time", "superior", "admin", "password"]
+  # end
 end
